9d9e24b73af30349e48a34243bf01e7164c7a605
wadiobao <dumabao69@gmail.com>
2024-03-19 23:41:14 +0700

    Update UI, add filter function, and add favicon.

--- bin/script.js
+++ bin/script.js
@@ -2,18 +2,22 @@ var slideIndex = 1;
 showDivs(slideIndex);
 
 function plusDivs(n) {
-  showDivs(slideIndex += n);
+  showDivs((slideIndex += n));
 }
 
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+  if (n > x.length) {
+    slideIndex = 1;
   }
-  x[slideIndex-1].style.display = "block";  
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
 }
 
 var myIndex = 0;
@@ -23,23 +27,48 @@ function carousel() {
   var i;
   var x = document.getElementsByClassName("mySlides");
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+    x[i].style.display = "none";
   }
   myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 5000);    
+  if (myIndex > x.length) {
+    myIndex = 1;
+  }
+  x[myIndex - 1].style.display = "block";
+  setTimeout(carousel, 5000);
 }
 
-function sbt(){
+function sbt() {
   document.getElementById("myForm").submit();
 }
 
 function dropDown(list) {
   var x = document.getElementById(list);
-  if (x.className.indexOf("w3-show") == -1) { 
+  if (x.className.indexOf("w3-show") == -1) {
     x.className += " w3-show";
   } else {
     x.className = x.className.replace(" w3-show", "");
-  }
+  }a
 }
+
+function filter(elementId) {
+  // Lắng nghe sự kiện click trên tất cả các ô đánh dấu
+  $(document).on("click", 'input[type="checkbox"]', function () {
+    // Lấy ra tất cả các ô đánh dấu trừ ô đang được click và gỡ chọn chúng
+    $('input[type="checkbox"]').not(this).prop("checked", false);
+    
+    // Kiểm tra xem ô đang được click có được chọn hay không
+    if ($(this).prop("checked")) {
+      // Ẩn tất cả các phần tử div trong #sp trừ phần tử có id là "elementId" và tất cả các div con của nó
+      $("#sp  div").not("#" + elementId + ", #" + elementId + "  *").hide();
+      // Hiển thị div có id là "elementId" và tất cả các div con bên trong nó
+      $("#" + elementId).show().find('div').show();
+    } else {
+      // Nếu không có ô đánh dấu nào được chọn, hiển thị tất cả các phần tử div trong #sp
+      $("#sp  div").show();
+    }
+  });
+}
+
+
+
+
